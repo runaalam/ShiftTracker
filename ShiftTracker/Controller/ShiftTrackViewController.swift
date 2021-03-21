510812a6526ba120e41fd783ceda35e0a1ea8cce
Runa Alam <runaalam@gmail.com>
2021-03-22 10:43:04 +1100

    Showing map and annotation, wrote toggle method for shift start and end button.

--- ShiftTracker/Controller/ShiftTrackViewController.swift
+++ ShiftTracker/Controller/ShiftTrackViewController.swift
@@ -9,24 +9,35 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ShiftTrackViewController: UIViewController, CLLocationManagerDelegate {
+class ShiftTrackViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     let widgetLocation = WidgetLocationManager()
     
     @IBOutlet weak var shiftStarButton: UIButton!
-    @IBOutlet weak var shiftEndButton: UIStackView!
+    @IBOutlet weak var shiftEndButton: UIButton!
+    @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        toggleShiftButton(senderButton: shiftEndButton, otherButton: shiftStarButton)
     }
         
     @IBAction func shiftStartButtonPressed(_ sender: Any) {
+        toggleShiftButton(senderButton: shiftStarButton, otherButton: shiftEndButton)
         let url = DeputyApiClient.Endpoints.shiftStart.url
         saveUserShift(url: url)
     }
     
     @IBAction func shiftEndButtonPressed(_ sender: Any) {
+        toggleShiftButton(senderButton: shiftEndButton, otherButton: shiftStarButton)
         let url = DeputyApiClient.Endpoints.shiftEnd.url
         saveUserShift(url: url)
+    }
+    
+    func toggleShiftButton(senderButton: UIButton, otherButton: UIButton){
+        senderButton.isEnabled = false
+        otherButton.isEnabled = true
     }
     
     func saveUserShift(url: URL) {
@@ -48,7 +59,9 @@ class ShiftTrackViewController: UIViewController, CLLocationManagerDelegate {
             let shift = Shift(time: shiftTime, latitude: shiftLat, longitude: shiftLon)
             completionHandler(shift)
         } else {
+            
             widgetLocation.fetchLocation(handler: { location in
+                self.addAnotationToMap(location: location)
                 shiftLat = String(location.coordinate.latitude)
                 shiftLon = String(location.coordinate.longitude)
                 let shift = Shift(time: shiftTime, latitude: shiftLat, longitude: shiftLon)
@@ -56,5 +69,31 @@ class ShiftTrackViewController: UIViewController, CLLocationManagerDelegate {
             })
         }
     }
+    
+    func addAnotationToMap(location: CLLocation){
+        let annotations = MKPointAnnotation()
+        annotations.coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+
+        self.mapView.addAnnotation(annotations)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            
+            let reuseId = "pin"
+            
+            var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+            
+            if pinView == nil {
+                pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+                pinView!.canShowCallout = true
+                pinView!.pinTintColor = .red
+                pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            }
+            else {
+                pinView!.annotation = annotation
+            }
+            
+            return pinView
+        }
 }
 
